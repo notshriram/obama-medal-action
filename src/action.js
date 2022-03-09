9dd9a26e8ddf1674a7076ff3c9c93ba5a7b4afc5
marirhs <18211a04l2@bvrit.ac.in>
2022-03-09 15:30:33 +0530

    Initial

---
+++ src/action.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { context } = require('@actions/github')
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+  
+const { pull_request } = context.payload;
+
+  
+async function run() {
+  const opened_name = pull_request.user.login;
+  const merged_name = pull_request.merged_by.login;  
+  let body = `hello ${opened_name} and ${merged_name} thanks for merging this feature`;
+  if(opened_name === merged_name) {
+    body = `Eyyyyy ![Self.png](https://user-images.githubusercontent.com/49370927/157414445-bfde117a-9748-4018-80b5-d356479a54cb.jpg)`;
+  }
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `${body}`
+  });
+}
+  
+run();
